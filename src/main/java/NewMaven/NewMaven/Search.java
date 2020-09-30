5da2307017efa570c5141c7b49d96c68a7d83c32
Azar5757 <mohdazaruddin1995@gmail.com>
2020-09-30 10:11:28 -0700

    adding add class

--- src/main/java/NewMaven/NewMaven/Search.java
+++ src/main/java/NewMaven/NewMaven/Search.java
@@ -1,5 +1,19 @@
 package NewMaven.NewMaven;
 
+import org.testng.annotations.Test;
+
 public class Search {
+	
+	
+	
+	@Test
+	public void sum() {
+		
+		System.out.println("sum");
+	}
+	@Test
+	public void add() {
+		System.out.println("add");
+	}
 
 }
