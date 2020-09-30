449606c11ffe24b775bbe031f62b3a930a54e1b1
Azar5757 <mohdazaruddin1995@gmail.com>
2020-09-30 10:27:03 -0700

    adding one new class

--- src/main/java/com/basic/Add.java
+++ src/main/java/com/basic/Add.java
@@ -1,5 +1,17 @@
 package com.basic;
 
+import org.testng.annotations.Test;
+
 public class Add {
+	
+	@Test
+	public void sum() {
+		
+		int i=10;
+		String j ="Azar";
+		int k =20;
+		System.out.println(i+j+k);
+		
+	}
 
 }
